eb370d2bf77a7f7481784088e875bffdeccc4355
rubin-aga <raga20@epoka.edu.al>
2023-04-28 13:26:41 +0200

    routing setup

--- assets/ts/app.tsx
+++ assets/ts/app.tsx
@@ -1,49 +1,43 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { createRoot } from 'react-dom/client'
-import { useBoundStore } from './store/index';
-
+import { useBoundStore } from './store/index'
 import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom'
+import { Home } from './screens/Home'
+import { NotFound } from './screens/NotFound'
 
-// import { useBoundStore } from 'store';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+])
 
 function App() {
-    const [tempState, setTempState] = useState<any[]>()
+  useEffect(() => {
+    axios.get('/api/pet_store/index').then(e => {
+      console.log(e)
+    })
+  }, [])
 
-    useEffect(() => {
-        axios.get('/api/pet_store/index').then(
-            e => {
-                setTempState(e.data)
-                console.log(e)
-            }
-        )
-    },[])
-
-
-    const testZ = useBoundStore((store) => store.bears)
-    const addBearFunction = useBoundStore((store) => store.addBear)
-        return (
-            <div>
-                {tempState &&
-                    tempState.map(pet => {
-                        console.log(pet)
-                        return(
-                            <div>
-                                <p className='bg-red-500 text-2xl border border-black'>{pet.id}</p>
-                                <p className='bg-green-500 text-lg text-white'>{pet.breed}</p>
-                                <p>{testZ}</p>
-                                <button onClick={addBearFunction}>CLICK</button>
-                            </div>
-                        )
-
-                        }
-                    )}
-                test52
-            </div>
-        );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
 }
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
