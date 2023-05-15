c53f3a569993cb752e48a3953378506c6759e2cc
Alissa Çenga <acenga20@epoka.edu.al>
2023-05-16 01:00:22 +0200

    implemented items and pets create crud

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -4,6 +4,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Entity\Item;
+use App\Entity\Pet;
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,16 +17,45 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/item')]
 class ItemController extends AbstractController
 {
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
     * @Route("/add-item", name="app_add_item", methods={"POST"})   
     * 
     */
     public function addItem(Request $request ) : JsonResponse
     {
-        $data = $request->getContent();
-        $data = json_decode($data, true);
-        dump($data);
-        return $this->json($data);
+            try{
+                $item = new Item();
+                $data = $request->getContent();
+                $data = json_decode($data, true);
+                $em  =$this->doctrine->getManager();
+                $user = $em->getRepository(User::class)->findOneBy(['id' => 1]);
+                $category = $em->getRepository(Category::class)->findOneBy(['name' => strtolower($data['category'])]);
+                $status = ($data['quantity'] > 0) ?  'has stock' : 'no stock';
+                $item->setUser($user);
+                $item->setName($data['name']);
+                $item->setQuantity($data['quantity']);
+                $item->setPrice($data['price']);
+                $item->setCategory($category);
+                $item->setStatus($status);
+                $item->setDescription($data['description']);
+                $item->setState($data['state']);
+                $item->setCreatedAt(1);
+                $item->setLastUpdatedAt(1);
+                $em->persist($item);
+                $em->flush();
+                return $this->json('success');
+
+            }catch(\Exception $exception){
+                return $this->json('failed');
+            }
+
 
     }
 
--- src/Controller/PetController.php
+++ src/Controller/PetController.php
@@ -4,6 +4,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Entity\Pet;
+use App\Entity\User;
+use App\Repository\PetRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,17 +16,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/pet')]
 class PetController extends AbstractController
-{
+{   private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+
     /**
     * @Route("/add-pet", name="app_add_pet", methods={"POST"})   
     * 
     */
     public function addPet(Request $request): JsonResponse
     {
-        $data = $request->getContent();
-        $data = json_decode($data, true);
-        dump($data);
-        return $this->json($data);
+        try{
+            $pet = new Pet();
+            $data = $request->getContent();
+            $data = json_decode($data, true);
+            $em  =$this->doctrine->getManager();
+            $user = $em->getRepository(User::class)->findOneBy(['id' => 1]);
+            $category = $em->getRepository(Category::class)->findOneBy(['name' => strtolower($data['category'])]);
+            $pet->setUser($user);
+            $pet->setName($data['name']);
+            $pet->setBreed($data['breed']);
+            $pet->setStatus($data['status']);
+            $pet->setCategory($category);
+            $pet->setDescription($data['description']);
+            $pet->setCreatedAt(1);
+            $pet->setLastUpdatedAt(1);
+            if($data['facts']){
+                $pet->setFacts($data['facts']);
+            }
+            $em->persist($pet);
+            $em->flush();
+            return $this->json('success');
+        }catch(\Exception $exception){
+            return $this->json('failed');
+        }
     }
 
 }
